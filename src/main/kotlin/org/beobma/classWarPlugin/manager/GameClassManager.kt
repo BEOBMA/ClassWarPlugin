f43667ecd2cdc8f68a35abc9229351d236205e2f
BEOBMA <ssdssdtkfk@naver.com>
2026-01-28 22:03:37 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/classWarPlugin/manager/GameClassManager.kt
+++ src/main/kotlin/org/beobma/classWarPlugin/manager/GameClassManager.kt
@@ -2,12 +2,14 @@ package org.beobma.classWarPlugin.manager
 
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.beobma.classWarPlugin.gameClass.Weapon
+import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 object GameClassManager {
     private val miniMessage = MiniMessage.miniMessage()
 
     fun Weapon.toItemStack(): ItemStack {
+        if (material == Material.AIR) return ItemStack(Material.AIR)
         val itemStack = ItemStack(material, 1).apply {
             itemMeta.apply {
                 displayName(miniMessage.deserialize(UtilManager.applyKeywords(name)))
