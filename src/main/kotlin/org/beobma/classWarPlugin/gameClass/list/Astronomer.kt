73ba39ac249e31432a15eb2fb70489d76e8416a9
BEOBMA <ssdssdssd601@gmail.com>
2026-02-05 10:27:36 +0900

    fix: 수정

--- src/main/kotlin/org/beobma/classWarPlugin/gameClass/list/Astronomer.kt
+++ src/main/kotlin/org/beobma/classWarPlugin/gameClass/list/Astronomer.kt
@@ -63,7 +63,9 @@ class AstronomersStaff : Weapon() {
 
 class AstronomersRedSkill : Skill() {
     override val name = "<bold>별자리"
-    override val description = listOf("<gray>8칸 내의 바라보는 적에게 5의 피해를 입힌다.")
+    override val description = listOf(
+        "<gray>8칸 내의 바라보는 적에게 5의 피해를 입힌다."
+    )
     override val cooldown = 10
 
     override fun use() {
@@ -119,20 +121,6 @@ class AstronomersOrangeSkill : Skill() {
     }
 }
 
-class AstronomersBlackHole : Flooring() {
-    override lateinit var location: Location
-    override var radius: Double = 5.0
-    override var targetType: TargetType = TargetType.Enemy
-    override var time: Int? = 4
-
-    override fun onFlooringEntityHit(hitEntityData: EntityData, location: Location) {
-        val hitEntity = hitEntityData.entity
-        val dir = location.clone().subtract(hitEntity.location).toVector().normalize().multiply(0.1)
-        hitEntity.velocity = dir
-        hitEntityData.damage(2.0, DamageType.Normal, playerData, false)
-    }
-}
-
 class AstronomersYellowSkill : Skill() {
     override val name = "<bold>별이 빛나는 밤"
     override val description = listOf(
@@ -153,6 +141,48 @@ class AstronomersYellowSkill : Skill() {
         teamField.location = currentLocation
         enemyField.spawnFlooring(playerData)
         teamField.spawnFlooring(playerData)
+    }
+}
+
+class AstronomersPassive : Passive(), OnHitHandler {
+    override val name = "<blue><bold>천문관측"
+    override val description = listOf(
+        "<gray>패시브",
+        "",
+        "<gray>{keyword:Mana} 회복 속도가 감소한다.",
+        "<gray>스킬 적중 시 {keyword:Mana}를 전부 소모하고 적중한 적 주변에 별을 떨어트린다.",
+        "<gray>떨어트리는 별의 수는 소모한 {keyword:Mana} 양에 비례하여 증가한다. (20당 1개, 최대 5개)",
+        "<gray>별은 적중한 적에게 1의 {keyword:TrueDamage}를 입힌다.",
+        "",
+        Keyword.TrueDamage.description ?: ""
+    )
+
+    override fun onSkillAttackHit(event: PlayerSkillDamageByPlayerEvent) {
+        if (event.damageType == DamageType.True) return
+        val mana = playerData.getOrCreateStatus(playerData) { Mana() }
+        val count = (mana.power / 20).coerceIn(1, 5)
+        val targetLoc = event.entity.entity.location.add(0.0, 5.0, 0.0)
+        repeat(count) {
+            val starMeteor = AstronomersStarMeteor()
+            starMeteor.location = targetLoc
+            starMeteor.spawnMeteor(playerData)
+        }
+        mana.updatePower(0)
+    }
+}
+
+
+class AstronomersBlackHole : Flooring() {
+    override lateinit var location: Location
+    override var radius: Double = 5.0
+    override var targetType: TargetType = TargetType.Enemy
+    override var time: Int? = 4
+
+    override fun onFlooringEntityHit(hitEntityData: EntityData, location: Location) {
+        val hitEntity = hitEntityData.entity
+        val dir = location.clone().subtract(hitEntity.location).toVector().normalize().multiply(0.1)
+        hitEntity.velocity = dir
+        hitEntityData.damage(2.0, DamageType.Normal, playerData, false)
     }
 }
 
@@ -214,33 +244,6 @@ class AstronomersTeamField : Flooring() {
 
     override fun onFlooringEnd() {
         affected.clear()
-    }
-}
-
-class AstronomersPassive : Passive(), OnHitHandler {
-    override val name = "<blue><bold>천문관측"
-    override val description = listOf(
-        "<gray>패시브",
-        "",
-        "<gray>{keyword:Mana} 회복 속도가 감소한다.",
-        "<gray>스킬 적중 시 {keyword:Mana}를 전부 소모하고 적중한 적 주변에 별을 떨어트린다.",
-        "<gray>떨어트리는 별의 수는 소모한 {keyword:Mana} 양에 비례하여 증가한다. (20당 1개, 최대 5개)",
-        "<gray>별은 적중한 적에게 1의 {keyword:TrueDamage}를 입힌다.",
-        "",
-        Keyword.TrueDamage.description ?: ""
-    )
-
-    override fun onSkillAttackHit(event: PlayerSkillDamageByPlayerEvent) {
-        if (event.damageType == DamageType.True) return
-        val mana = playerData.getOrCreateStatus(playerData) { Mana() }
-        val count = (mana.power / 20).coerceIn(1, 5)
-        val targetLoc = event.entity.entity.location.add(0.0, 5.0, 0.0)
-        repeat(count) {
-            val starMeteor = AstronomersStarMeteor()
-            starMeteor.location = targetLoc
-            starMeteor.spawnMeteor(playerData)
-        }
-        mana.updatePower(0)
     }
 }
 
